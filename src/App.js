022b5ae0d58c129c59eefedc695e3ab5492a044d
MoNRaSpGit <ju4nrsuarez@gmail.com>
2024-03-21 18:16:10 -0300

    Abre la camara y lee el codigo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from 'react';
+import Quagga from 'quagga'; // Importa la biblioteca de escaneo de códigos de barras
 
-function App() {
+const BarcodeScanner = () => {
+  const videoRef = useRef(null);
+  const [scannedResult, setScannedResult] = useState('');
+
+  // Función para activar la cámara y comenzar el escaneo
+  const startScanner = () => {
+    Quagga.init({
+      inputStream: {
+        name: "Live",
+        type: "LiveStream",
+        target: videoRef.current
+      },
+      decoder: {
+        readers: ["ean_reader"] // Especifica el tipo de código de barras que deseas escanear
+      }
+    }, function(err) {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log("Initialization finished. Ready to start");
+      Quagga.start();
+    });
+
+    // Función para manejar el resultado del escaneo
+    Quagga.onDetected(data => {
+      setScannedResult(data.codeResult.code);
+      Quagga.stop();
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         hola q tal
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Barcode Scanner</h1>
+      <video ref={videoRef} />
+      <button onClick={startScanner}>Start Scanning</button>
+      <p>Scanned Result: {scannedResult}</p>
     </div>
   );
-}
+};
 
-export default App;
+export default BarcodeScanner;
